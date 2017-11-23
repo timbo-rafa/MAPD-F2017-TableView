7fa198976824ba997b0cbc6c95f0284a94886dbe
Student <student@setass-mac-mini-2.local>
2017-11-22 19:58:59 -0500

    created simple table view structure

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -1,12 +1,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    private let movies = ["Batman vs Superman", "Blade Runner 2049", "Thor: Ragnarok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi"]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCellStyle.default,
+                reuseIdentifier: simpleTableIdentifier)
+        }
+        cell?.textLabel?.text = movies[indexPath.row]
+        return cell!
+    }
 
 }
 
