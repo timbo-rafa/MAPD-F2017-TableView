335b39c3505b8612b90fbdf1092f5c5b4b543898
Student <student@setass-mac-mini-2.local>
2017-11-22 20:16:26 -0500

    table view style, image

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -3,7 +3,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    private let movies = ["Batman vs Superman", "Blade Runner 2049", "Thor: Ragnarok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi"]
+    private let movies = ["Batman vs Superman", "Thor: Ragnarok", "Justice League", "Wonderwoman",
+                          "Captain America: Civil War",
+                          "Dark Tower", "Star Wars: The Last Jedi", "Blade Runner 2049"]
     let simpleTableIdentifier = "SimpleTableIdentifier"
     
     override func viewDidLoad() {
@@ -18,9 +20,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
         if (cell == nil) {
             cell = UITableViewCell(
-                style: UITableViewCellStyle.default,
+                style: UITableViewCellStyle.value2,
                 reuseIdentifier: simpleTableIdentifier)
         }
+        
+        let image = UIImage(named: "star")
+        cell?.imageView?.image = image
+        let highlightedImage = UIImage(named: "star2")
+        cell?.imageView?.highlightedImage = highlightedImage
+        
+        if indexPath.row < 5 {
+            cell?.detailTextLabel?.text = "Superhero Movies"
+        } else {
+            cell?.detailTextLabel?.text = "Sci-Fi Movies"
+        }
+        
         cell?.textLabel?.text = movies[indexPath.row]
         return cell!
     }
